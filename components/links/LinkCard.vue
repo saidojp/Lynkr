244e275056b741a4d2ce8be89d071257611936dc
saidojp <saidlowxp@gmail.com>
2025-07-26 17:43:19 +0500

    bug fix

--- components/links/LinkCard.vue
+++ components/links/LinkCard.vue
@@ -218,6 +218,7 @@ const formatDate = (dateString: string) => {
 .line-clamp-2 {
   display: -webkit-box;
   -webkit-line-clamp: 2;
+  line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
 }
@@ -225,6 +226,7 @@ const formatDate = (dateString: string) => {
 .line-clamp-3 {
   display: -webkit-box;
   -webkit-line-clamp: 3;
+  line-clamp: 3;
   -webkit-box-orient: vertical;
   overflow: hidden;
 }
